52d7c2e9f4f5248d7dc8fdb64b06b9977d3ceb3f
Rodrigo Petrizzo <petrizzo.rodrigoa@gmail.com>
2020-12-29 10:12:37 -0300

    Add files via upload

---
+++ BruceForce.py
@@ -0,0 +1,26 @@
+import itertools
+import string 
+from win32com.client import Dispatch
+import time
+
+file = input('Path: ')
+
+chars = string.ascii_lowercase + string.digits + string.ascii_uppercase
+
+for password_length in range(6, 20):
+    for password in itertools.product(chars, repeat=password_length):
+        password = ''.join(password)
+
+        #print ('Testing password: '+ password)
+        instance = Dispatch ('Excel.Application')
+
+        try:
+            instance.Workbooks.Open(file, False, True, None, password)
+            sleep(0.1)
+            print ('Password Cracked: ' + password)
+
+            break
+
+        except:
+            pass
+    break      
